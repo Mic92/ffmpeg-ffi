a341f23fdb5729647b9f9d4a4f5fda1de38463fd
Kohei Suzuki <eagletmt@gmail.com>
2014-01-29 19:47:00 +0900

    Fill in AVCodec fields

--- lib/ffmpeg-ffi/c/codec.rb
+++ lib/ffmpeg-ffi/c/codec.rb
@@ -1,13 +1,42 @@
-require 'ffi'
+require 'ffmpeg-ffi'
 
 module FFmpeg
   module C
     class Codec < FFI::Struct
-      layout(
+      FF_API_LOWRES = FFmpeg.avcodec_version[0] < 56
+
+      fields = [
         :name, :string,
         :long_name, :string,
-        # and more...
-      )
+        :type, :int,
+        :id, :int,
+        :capabilities, :int,
+        :supported_framerates, Rational.by_ref,
+        :pix_fmts, :int,
+        :supported_samplerates, :pointer,
+        :sample_fmts, :int,
+        :channel_layouts, :uint64,
+      ]
+      if FF_API_LOWRES
+        fields += [:max_lowres, :uint8]
+      end
+      fields += [
+        :priv_class, :pointer,
+        :profiles, :pointer,
+        :priv_data_size, :int,
+        :next, Codec.by_ref,
+        :init_thread_copy, :pointer,
+        :update_thread_context, :pointer,
+        :defaults, :pointer,
+        :init_static_data, :pointer,
+        :init, :pointer,
+        :encode_sub, :pointer,
+        :encode2, :pointer,
+        :decode, :pointer,
+        :close, :pointer,
+        :flush, :pointer,
+      ]
+      layout(*fields)
     end
   end
 end
